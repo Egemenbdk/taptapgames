f61870ea582a2143cc518f83ce1f0111d553f544
Egemenbdk <70774662+Egemenbdk@users.noreply.github.com>
2023-09-17 22:32:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,67 +6,67 @@ document.addEventListener('DOMContentLoaded', function () {
         {
             title: 'Football Legends',
             imageUrl: 'https://images.crazygames.com/games/soccer-legends-2021/cover-1619606990485.png?auto=format%2Ccompress&q=45&cs=strip&ch=DPR&w=1200&h=630&fit=crop',
-            url: 'football-legends', // Add game URL here
+            url: 'football-legends/index.html', // Add game URL here
         },
         {
             title: 'Penalty Kick Online',
             imageUrl: 'https://www.coolmathgames.com/sites/default/files/PenaltyKickOnline_OG-logo.jpg',
-            url: 'penalty-kick-online', // Add game URL here
+            url: 'penalty-kick-online/index.html', // Add game URL here
         },
         {
             title: 'Retro Bowl',
             imageUrl: 'https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/en_US/games/switch/r/retro-bowl-switch/hero',
-            url: 'retro-bowl', // Add game URL here
+            url: 'retro-bowl/index.html', // Add game URL here
         },
         {
             title: "Papa's Pizzeria",
             imageUrl: 'https://tcf.admeen.org/game/5500/5489/400x246/papa-s-pizzeria.jpg',
-            url: 'papas-pizzeria', // Add game URL here
+            url: 'papas-pizzeria/index.html', // Add game URL here
         },
         {
             title: 'Fireboy and Watergirl',
             imageUrl: 'https://images.crazygames.com/games/fireboy-and-watergirl-the-forest-temple/cover-1586285142530.jpg?auto=format,compress&q=75&cs=strip',
-            url: 'fireboy-and-watergirl', // Add game URL here
+            url: 'fireboy-and-watergirl/index.html', // Add game URL here
         },
         {
             title: 'Basketball Stars',
             imageUrl: 'https://images.crazygames.com/games/basketball-stars-2019/cover-1583231506155.png?auto=format%2Ccompress&q=45&cs=strip&ch=DPR&w=1200&h=630&fit=crop',
-            url: 'basketball-stars', // Add game URL here
+            url: 'basketball-stars/index.html', // Add game URL here
         },
         {
             title: 'Smash Karts',
             imageUrl: 'https://images.crazygames.com/games/smash-karts/cover-1690365923966.png?auto=format,compress&q=75&cs=strip',
-            url: 'smash-karts', // Add game URL here
+            url: 'smash-karts/index.html', // Add game URL here
         },
         {
             title: 'Sandy Balls',
             imageUrl: 'https://m.media-amazon.com/images/I/71Z4OiWOHKL.png',
-            url: 'sandy-balls', // Add game URL here
+            url: 'sandy-balls/index.html', // Add game URL here
         },
         {
             title: 'Water Color Sort',
             imageUrl: 'https://www.primarygames.com/puzzles/strategy/watersort/logo200.png',
-            url: 'water-color-sort', // Add game URL here
+            url: 'water-color-sort/index.html', // Add game URL here
         },
         {
             title: 'Dunk Shot',
             imageUrl: 'https://static.gamezop.com/S1Ne12TQqCH/cover.jpg',
-            url: 'dunk-shot', // Add game URL here
+            url: 'dunk-shot/index.html', // Add game URL here
         },
         {
             title: 'Blazing Blades',
             imageUrl: 'https://static.gamezop.com/UYiznUAya/cover.jpg',
-            url: 'blazing-blades', // Add game URL here
+            url: 'blazing-blades/index.html', // Add game URL here
         },
         {
             title: 'JustFall.LOL',
             imageUrl: 'https://games.lol/wp-content/uploads/2023/07/just-fall-lol-pc-full-version.jpg',
-            url: 'justfall-lol', // Add game URL here
+            url: 'justfall-lol/index.html', // Add game URL here
         },
         {
             title: 'Scribble World Drawing',
             imageUrl: 'https://i.ibb.co/jw0gHVV/download-2.jpg',
-            url: 'scribble-world-drawing', // Add game URL here
+            url: 'scribble-world-drawing/index.html', // Add game URL here
         },
         // Add more game data objects as needed
     ];
